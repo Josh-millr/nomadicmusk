c4f402881a8ca0386864a6d564a70534393a6969
josh-miller <joshuae.miller100@gmail.com>
2022-11-05 23:37:21 +0100

    Added attribute to open new tab on click home icon

--- components/TopNav/TopNav.jsx
+++ components/TopNav/TopNav.jsx
@@ -4,7 +4,7 @@ export const TopNav = () => {
   return (
     <nav class="fixed top-0 left-0 z-20 w-full border-b border-gray-900 px-2 py-2.5 dark:border-gray-600 dark:bg-gray-900 sm:px-4">
       <div class="container mx-auto flex flex-wrap items-center justify-between">
-        <a href="https://unknown.com/" class="flex items-center">
+        <a href="https://unknown.com/" target="blank" class="flex items-center">
           <Logo width="24" height="24" color="white"/>
         </a>
         <div class="flex items-center gap-x-[16px] md:order-2">
