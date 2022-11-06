b2eda102e829256c9aede7a7ac8debf1d5bf9eb9
josh-miller <joshuae.miller100@gmail.com>
2022-11-06 01:36:50 +0100

    Added sign in form title

--- components/FormInput/FormInput.jsx
+++ components/FormInput/FormInput.jsx
@@ -5,7 +5,7 @@ export const FormInput = ({ type, placeholder, required, getValue, label }) => {
     <div className="grid grid-cols-1 grid-rows-1 gap-y-[0px]">
       <label
         htmlFor={type}
-        className="mb-2 block text-xs font-medium text-white dark:text-gray-300 sm:text-sm"
+        className="mb-2 block text-sm font-medium text-white dark:text-gray-300"
       >
         {label}
       </label>
--- pages/signin.js
+++ pages/signin.js
@@ -2,8 +2,14 @@ import { FormInput, Checkbox, Button } from "../components/index";
 
 export default function SignIn() {
   return (
-    <div className="my-[16vh] w-full px-[24px] sm:grid sm:justify-center">
-      <form className="grid-row-1 grid grid-cols-1 justify-items-center sm:w-[420px]">
+    <div className="grid w-full grid-cols-1 grid-rows-1 justify-items-center gap-y-[40px] py-[16vh] px-[24px] sm:grid sm:justify-center">
+      <div className="grid grid-cols-1 grid-rows-1 justify-items-center  gap-y-[8px]">
+        <h1 className="text-3xl font-bold text-[#ffffff]">Sign In</h1>
+        <p className="text-xs text-[#95959f] sm:text-xs">
+          Sign in to the platform to get started
+        </p>
+      </div>
+      <form className="grid-row-1 grid w-full grid-cols-1 justify-items-center sm:w-[420px]">
         <div className="mb-[16px] w-full">
           <FormInput
             label="Your Email"
