327dedb4f21957ea0c03a4412d87e9a4384cf488
josh-miller <joshuae.miller100@gmail.com>
2022-11-06 01:21:40 +0100

    Added button to signup form

--- pages/signin.js
+++ pages/signin.js
@@ -1,4 +1,4 @@
-import { FormInput, Checkbox } from "../components/index";
+import { FormInput, Checkbox, Button } from "../components/index";
 
 export default function SignIn() {
   return (
@@ -25,12 +25,7 @@ export default function SignIn() {
         <div className="mb-[16px] w-full">
           <Checkbox label="Remember me" />
         </div>
-        <button
-          type="submit"
-          className="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto"
-        >
-          Submit
-        </button>
+        <Button.Default label="Submit" />
       </form>
     </div>
   );
