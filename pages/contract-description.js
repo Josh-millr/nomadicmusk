77ee1ffa7a3d1e6729d6b581b147582cb9153b05
josh-miller <joshuae.miller100@gmail.com>
2022-11-12 18:19:29 +0100

    Added Contract creation info layout

--- pages/contract-description.js
+++ pages/contract-description.js
@@ -16,6 +16,16 @@ const ContractActionButton = ({ label, icon: Icon }) => {
   );
 };
 
+const ContractItem = ({ prop, value }) => {
+  return (
+    <div className="flex w-full items-center justify-between border-b border-gray-300 pb-2">
+      <p className="font-bold">{prop}</p>
+      <div className="h-fit w-fit rounded-full border bg-gray-300 px-4 py-2 text-gray-700">
+        <p className="whitespace-nowrap text-xs font-bold">{value}</p>
+      </div>
+    </div>
+  );
+};
 const ContractDescription = () => {
   return (
     <div className="px-6 md:px-10 lg:px-20">
@@ -68,6 +78,23 @@ const ContractDescription = () => {
               </p>
             </div>
           </div>
+        </div>
+      </div>
+
+      {/* Contract Creation Info */}
+      <div className="my-6 flex h-fit flex-col gap-x-6 gap-y-2 rounded-3xl bg-gray-200 p-6 py-10 text-xs text-gray-800 lg:flex-row lg:bg-transparent lg:py-0">
+        <div className="grid w-full grid-cols-1 grid-rows-1 gap-y-2 rounded-3xl lg:bg-gray-200 lg:p-6">
+          {/* Contract Item */}
+          <ContractItem prop="Contract Address" value=" 0x47378....67dfc" />
+          <ContractItem prop="Blockchain" value="Ethereum" />
+          <ContractItem prop="Block Number:" value="2342355" />
+        </div>
+        {/* ................................... */}
+        <div className="grid w-full grid-cols-1 grid-rows-1 gap-y-2 rounded-3xl lg:bg-gray-200 lg:p-6">
+          {/* Contract Item */}
+          <ContractItem prop="Token Standard" value="ERC-721" />
+          <ContractItem prop="Blockchain" value="Ethereum" />
+          <ContractItem prop="Block Number:" value="2342355" />
         </div>
       </div>
     </div>
